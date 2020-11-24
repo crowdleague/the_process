efd8f6a7df98d0aca0b9772fa7ed1b7b7ce6a16b
Nick Meinhold <nick.meinhold@gmail.com>
2020-11-24 20:54:29 +1100

    On first sign in, add profile data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import { saveDetails } from './auth/on_first_sign_in';
 import { exchangeCodeWithGoogle } from './auth/exchange_with_google';
 import { redirectToAuthorization } from './auth/redirect_to_authorization';
 
@@ -6,6 +7,8 @@ import { redirectToAuthorization } from './auth/redirect_to_authorization';
 export const getAuthorized = functions.https.onRequest(redirectToAuthorization);
 export const exchangeWithGoogle = functions.https.onRequest(exchangeCodeWithGoogle);
 
+// when a new account is created, add auth details to the database
+export const saveDetailsOnFirstSignIn = functions.auth.user().onCreate(saveDetails);
 
 // Save the token to a document named as the user id
     // const dbEntry = new database.AuthToken(req.query.state, token_response);
