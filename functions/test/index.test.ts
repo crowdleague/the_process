707bd659ef4aecf59a4d8dafa523b7b551d437f2
Nick Meinhold <nick.meinhold@gmail.com>
2020-12-17 00:18:12 +1100

    Setup for unit testing cloud functions
    - added chai and mocha with types for running tests
    - added ts-node for running tests without having to compile
    - added ts-sinon for mocking

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -1,1 +1,23 @@
-import test = require('firebase-functions-test')();
+import * as funcTest from "firebase-functions-test";
+import * as admin from 'firebase-admin';
+import { stubObject } from "ts-sinon";
+
+describe("firebase cloud functions", () => {
+  let myFunctions : any, adminInitStub : any;
+  const tester = funcTest();
+  before(async () => {
+    adminInitStub = stubObject<any>(admin, { method: "initializeApp" });
+    myFunctions = await import("../src/index");
+  });
+
+  it("createSectionFolder", () => {
+    let input: any;
+    const wrapped = tester.wrap(myFunctions.createSectionFolder(input, context));
+    wrapped(null);
+  });
+
+  after(() => {
+    // adminInitStub.restore();
+    tester.cleanup();
+  });
+});
