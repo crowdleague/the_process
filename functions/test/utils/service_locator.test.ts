7b06eaa9622823566055fbc12af3ca2c3869883a
Nick Meinhold <nick.meinhold@gmail.com>
2020-12-26 00:15:29 +1100

    Update service_locator function signature

--- functions/test/utils/service_locator.test.ts
+++ functions/test/utils/service_locator.test.ts
@@ -13,7 +13,10 @@ describe('Service Locator', () => {
   });
 
   it('locates services with expected properties', async () => {
-    const sectionData = service_locator.createSectionData({uid: 'uid', name: 'name', folderId: 'folderId', useCasesDocId: 'useCasesDocId'});
+    const sectionData = service_locator.createSectionData('uid');
+    sectionData.name = 'name';
+    sectionData.folderId = 'folderId';
+    sectionData.useCasesDocId = 'useCasesDocId';
     expect(sectionData.uid).equals('uid');
     expect(sectionData.name).equals('name');
     expect(sectionData.folderId).equals('folderId');
