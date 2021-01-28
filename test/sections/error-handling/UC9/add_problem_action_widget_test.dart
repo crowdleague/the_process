e323d65126649b5f6d9cf659cb3b57a43b06c1c3
Nick Meinhold <nick.meinhold@gmail.com>
2021-01-28 17:22:12 +1100

    Fixed problematic map structure
    - changed to iterating and checking for each type to allow conversion
    between a type and the corresponding page data object

--- test/sections/error-handling/UC9/add_problem_action_widget_test.dart
+++ test/sections/error-handling/UC9/add_problem_action_widget_test.dart
@@ -1,20 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:the_process/actions/problems/add_problem.dart';
-import 'package:the_process/widgets/app_widget/app_widget.dart';
+import 'package:the_process/models/navigation/page_data/problem_page_data.dart';
+import 'package:the_process/models/problems/problem.dart';
 import 'package:the_process/widgets/shared/problem_page.dart';
 
-import '../../../utils/widget_test_harness.dart';
+import '../../../mocks/redux/fake_store.dart';
+import '../../../utils/testing/app_widget_harness.dart';
 
 void main() {
   group('AddProblemAction', () {
     testWidgets('Adds ProblemPage to widget tree', (WidgetTester tester) async {
-      final wig = WidgetTestHarness(widgetUnderTest: AppWidget());
-      await tester.pumpWidget(wig);
-
       final errorString = 'Problem error message';
+      final problem = Problem(errorString: errorString);
+      final problemPageData = ProblemPageData(problem: problem);
 
-      // need stream controller
-      AddProblem(errorString: errorString);
+      final store =
+          FakeStore(updates: (b) => b..pagesData.add(problemPageData));
+      final widget = AppWidgetHarness(store: store).widget;
+
+      await tester.pumpWidget(widget);
+
+      await tester.pump();
 
       final problemPage = find.byType(ProblemPage);
 
