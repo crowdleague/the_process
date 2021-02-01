9041efd5219e9838dc4b2f926910ba75d2028631
Nick Meinhold <nick.meinhold@gmail.com>
2021-02-01 20:19:11 +1100

    Create authenticated client

--- dart_microservices/create_section/lib/src/services/authentication_service.dart
+++ dart_microservices/create_section/lib/src/services/authentication_service.dart
@@ -1,10 +1,12 @@
 import 'package:googleapis/firestore/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
 
+import 'package:shared_models/shared_models.dart' show GoogleUserCredentials;
+
 class AuthenticationService {
   AuthenticationService();
 
-  void setup() async {
+  static Future<GoogleUserCredentials> getGoogleUserCredentials() async {
     final autoRefreshingClient =
         await clientViaApplicationDefaultCredentials(scopes: []);
 
@@ -16,6 +18,17 @@ class AuthenticationService {
     final credentialsDoc = await firestoreApi.projects.databases.documents
         .get(credentialsDocumentName);
 
-    final jsonMap = credentialsDoc.fields['google']!.toJson();
+    final googleFields = credentialsDoc.fields['google']!.mapValue.fields;
+
+    final credentials = GoogleUserCredentials(
+      accessToken: googleFields['access_token']!.stringValue,
+      refreshToken: googleFields['refresh_token']!.stringValue,
+      expiryDate: int.parse(googleFields['expiry_date']!.integerValue),
+      idToken: googleFields['id_token']!.stringValue,
+      tokenType: googleFields['token_type']!.stringValue,
+      scope: googleFields['scope']!.stringValue,
+    );
+
+    return credentials;
   }
 }
--- dart_microservices/create_section/test/e2e/drive_api_test.dart
+++ dart_microservices/create_section/test/e2e/drive_api_test.dart
@@ -1,15 +1,33 @@
 import 'dart:io';
 
+import 'package:create_section/src/services/authentication_service.dart';
 import 'package:googleapis/drive/v3.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:test/test.dart';
+import 'package:http/io_client.dart' as http;
 
 void main() {
   test('DriveAPI', () async {
-    final autoRefreshingClient = await clientViaApplicationDefaultCredentials(
-        scopes: ['https://www.googleapis.com/auth/drive']);
+    final userCredentials =
+        await AuthenticationService.getGoogleUserCredentials();
 
-    final driveApi = DriveApi(autoRefreshingClient);
+    final accessToken = AccessToken(
+        userCredentials.tokenType,
+        userCredentials.accessToken,
+        DateTime.fromMillisecondsSinceEpoch(userCredentials.expiryDate)
+            .toUtc());
+
+    //
+
+    final accessCredentials = AccessCredentials(accessToken,
+        userCredentials.refreshToken, userCredentials.scope.split(' '));
+
+    final authClient = authenticatedClient(http.IOClient(), accessCredentials);
+
+    // final autoRefreshingClient = await clientViaApplicationDefaultCredentials(
+    //     scopes: ['https://www.googleapis.com/auth/drive']);
+
+    final driveApi = DriveApi(authClient);
 
     final newFile = File()
       ..name = 'testName'
