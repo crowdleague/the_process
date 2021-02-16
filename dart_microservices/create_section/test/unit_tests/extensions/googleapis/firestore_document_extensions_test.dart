5a32da77688000fc268edbc3016c52cdb226204f
Nick Meinhold <nick.meinhold@gmail.com>
2021-02-17 10:36:44 +1100

    Fix firestore document extensions test

--- dart_microservices/create_section/test/unit_tests/extensions/googleapis/firestore_document_extensions_test.dart
+++ dart_microservices/create_section/test/unit_tests/extensions/googleapis/firestore_document_extensions_test.dart
@@ -1,28 +1,31 @@
-import 'package:googleapis/firestore/v1.dart';
 import 'package:test/test.dart';
 
 import 'package:create_section/src/extensions/googleapis/firestore_document_extensions.dart';
+
+import '../../../test_data/firestore_document_test_data.dart'
+    as firestore_document_test_data;
 
 void main() {
   group('DocumentExtension', () {
     test('toGoogleUserCredentials() returns expected GoogleUserCredentials',
         () {
-      //   final example fieldsMapValue = Value()..mapValue = MapValue()..
-      //   final exampleDocument = Document()..fields = ..;
+      final doc = firestore_document_test_data.userCredentialsDoc;
 
-      //   final googleFields = fields['google']!.mapValue.fields;
+      final googleUserCredentials = doc.toGoogleUserCredentials();
 
-      // return GoogleUserCredentials(
-      //   accessToken: googleFields['access_token']!.stringValue,
-      //   refreshToken: googleFields['refresh_token']!.stringValue,
-      //   expiryDate: int.parse(googleFields['expiry_date']!.integerValue),
-      //   idToken: googleFields['id_token']!.stringValue,
-      //   tokenType: googleFields['token_type']!.stringValue,
-      //   scope: googleFields['scope']!.stringValue,
+      final googleFields = doc.fields['google']!.mapValue.fields;
 
-      //   final value = exampleInt.asValue();
-
-      //   expect(value.integerValue, exampleInt.toString());
+      expect(googleUserCredentials.accessToken,
+          googleFields['access_token']!.stringValue);
+      expect(googleUserCredentials.refreshToken,
+          googleFields['refresh_token']!.stringValue);
+      expect(googleUserCredentials.expiryDate,
+          int.parse(googleFields['expiry_date']!.integerValue));
+      expect(
+          googleUserCredentials.idToken, googleFields['id_token']!.stringValue);
+      expect(googleUserCredentials.tokenType,
+          googleFields['token_type']!.stringValue);
+      expect(googleUserCredentials.scope, googleFields['scope']!.stringValue);
     });
   });
 }
