95f6465d16e77393d66551d696a656a792972cdd
Nick Meinhold <nick.meinhold@gmail.com>
2021-02-20 11:47:06 +1100

    Run build_runner

--- shared_models/lib/src/google_user_credentials.freezed.dart
+++ shared_models/lib/src/google_user_credentials.freezed.dart
@@ -44,7 +44,6 @@ const $GoogleUserCredentials = _$GoogleUserCredentialsTearOff();
 
 /// @nodoc
 mixin _$GoogleUserCredentials {
-<<<<<<< HEAD
   @JsonKey(name: 'refresh_token')
   String get refreshToken;
   @JsonKey(name: 'expiry_date')
@@ -55,13 +54,6 @@ mixin _$GoogleUserCredentials {
   String get tokenType;
   @JsonKey(name: 'id_token')
   String get idToken;
-=======
-  String get refresh_token;
-  int get expiry_date;
-  String get access_token;
-  String get token_type;
-  String get id_token;
->>>>>>> dev
   String get scope;
 
   Map<String, dynamic> toJson();
@@ -75,19 +67,11 @@ abstract class $GoogleUserCredentialsCopyWith<$Res> {
           $Res Function(GoogleUserCredentials) then) =
       _$GoogleUserCredentialsCopyWithImpl<$Res>;
   $Res call(
-<<<<<<< HEAD
       {@JsonKey(name: 'refresh_token') String refreshToken,
       @JsonKey(name: 'expiry_date') int expiryDate,
       @JsonKey(name: 'access_token') String accessToken,
       @JsonKey(name: 'token_type') String tokenType,
       @JsonKey(name: 'id_token') String idToken,
-=======
-      {String refresh_token,
-      int expiry_date,
-      String access_token,
-      String token_type,
-      String id_token,
->>>>>>> dev
       String scope});
 }
 
@@ -102,7 +86,6 @@ class _$GoogleUserCredentialsCopyWithImpl<$Res>
 
   @override
   $Res call({
-<<<<<<< HEAD
     Object? refreshToken = freezed,
     Object? expiryDate = freezed,
     Object? accessToken = freezed,
@@ -119,27 +102,6 @@ class _$GoogleUserCredentialsCopyWithImpl<$Res>
           accessToken == freezed ? _value.accessToken : accessToken as String,
       tokenType: tokenType == freezed ? _value.tokenType : tokenType as String,
       idToken: idToken == freezed ? _value.idToken : idToken as String,
-=======
-    Object? refresh_token = freezed,
-    Object? expiry_date = freezed,
-    Object? access_token = freezed,
-    Object? token_type = freezed,
-    Object? id_token = freezed,
-    Object? scope = freezed,
-  }) {
-    return _then(_value.copyWith(
-      refresh_token: refresh_token == freezed
-          ? _value.refresh_token
-          : refresh_token as String,
-      expiry_date:
-          expiry_date == freezed ? _value.expiry_date : expiry_date as int,
-      access_token: access_token == freezed
-          ? _value.access_token
-          : access_token as String,
-      token_type:
-          token_type == freezed ? _value.token_type : token_type as String,
-      id_token: id_token == freezed ? _value.id_token : id_token as String,
->>>>>>> dev
       scope: scope == freezed ? _value.scope : scope as String,
     ));
   }
@@ -153,19 +115,11 @@ abstract class _$GoogleUserCredentialsCopyWith<$Res>
       __$GoogleUserCredentialsCopyWithImpl<$Res>;
   @override
   $Res call(
-<<<<<<< HEAD
       {@JsonKey(name: 'refresh_token') String refreshToken,
       @JsonKey(name: 'expiry_date') int expiryDate,
       @JsonKey(name: 'access_token') String accessToken,
       @JsonKey(name: 'token_type') String tokenType,
       @JsonKey(name: 'id_token') String idToken,
-=======
-      {String refresh_token,
-      int expiry_date,
-      String access_token,
-      String token_type,
-      String id_token,
->>>>>>> dev
       String scope});
 }
 
@@ -182,7 +136,6 @@ class __$GoogleUserCredentialsCopyWithImpl<$Res>
 
   @override
   $Res call({
-<<<<<<< HEAD
     Object? refreshToken = freezed,
     Object? expiryDate = freezed,
     Object? accessToken = freezed,
@@ -199,27 +152,6 @@ class __$GoogleUserCredentialsCopyWithImpl<$Res>
           accessToken == freezed ? _value.accessToken : accessToken as String,
       tokenType: tokenType == freezed ? _value.tokenType : tokenType as String,
       idToken: idToken == freezed ? _value.idToken : idToken as String,
-=======
-    Object? refresh_token = freezed,
-    Object? expiry_date = freezed,
-    Object? access_token = freezed,
-    Object? token_type = freezed,
-    Object? id_token = freezed,
-    Object? scope = freezed,
-  }) {
-    return _then(_GoogleUserCredentials(
-      refresh_token: refresh_token == freezed
-          ? _value.refresh_token
-          : refresh_token as String,
-      expiry_date:
-          expiry_date == freezed ? _value.expiry_date : expiry_date as int,
-      access_token: access_token == freezed
-          ? _value.access_token
-          : access_token as String,
-      token_type:
-          token_type == freezed ? _value.token_type : token_type as String,
-      id_token: id_token == freezed ? _value.id_token : id_token as String,
->>>>>>> dev
       scope: scope == freezed ? _value.scope : scope as String,
     ));
   }
@@ -230,26 +162,17 @@ class __$GoogleUserCredentialsCopyWithImpl<$Res>
 /// @nodoc
 class _$_GoogleUserCredentials implements _GoogleUserCredentials {
   _$_GoogleUserCredentials(
-<<<<<<< HEAD
       {@JsonKey(name: 'refresh_token') required this.refreshToken,
       @JsonKey(name: 'expiry_date') required this.expiryDate,
       @JsonKey(name: 'access_token') required this.accessToken,
       @JsonKey(name: 'token_type') required this.tokenType,
       @JsonKey(name: 'id_token') required this.idToken,
-=======
-      {required this.refresh_token,
-      required this.expiry_date,
-      required this.access_token,
-      required this.token_type,
-      required this.id_token,
->>>>>>> dev
       required this.scope});
 
   factory _$_GoogleUserCredentials.fromJson(Map<String, dynamic> json) =>
       _$_$_GoogleUserCredentialsFromJson(json);
 
   @override
-<<<<<<< HEAD
   @JsonKey(name: 'refresh_token')
   final String refreshToken;
   @override
@@ -264,34 +187,18 @@ class _$_GoogleUserCredentials implements _GoogleUserCredentials {
   @override
   @JsonKey(name: 'id_token')
   final String idToken;
-=======
-  final String refresh_token;
-  @override
-  final int expiry_date;
-  @override
-  final String access_token;
-  @override
-  final String token_type;
-  @override
-  final String id_token;
->>>>>>> dev
   @override
   final String scope;
 
   @override
   String toString() {
-<<<<<<< HEAD
     return 'GoogleUserCredentials(refreshToken: $refreshToken, expiryDate: $expiryDate, accessToken: $accessToken, tokenType: $tokenType, idToken: $idToken, scope: $scope)';
-=======
-    return 'GoogleUserCredentials(refresh_token: $refresh_token, expiry_date: $expiry_date, access_token: $access_token, token_type: $token_type, id_token: $id_token, scope: $scope)';
->>>>>>> dev
   }
 
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _GoogleUserCredentials &&
-<<<<<<< HEAD
             (identical(other.refreshToken, refreshToken) ||
                 const DeepCollectionEquality()
                     .equals(other.refreshToken, refreshToken)) &&
@@ -307,23 +214,6 @@ class _$_GoogleUserCredentials implements _GoogleUserCredentials {
             (identical(other.idToken, idToken) ||
                 const DeepCollectionEquality()
                     .equals(other.idToken, idToken)) &&
-=======
-            (identical(other.refresh_token, refresh_token) ||
-                const DeepCollectionEquality()
-                    .equals(other.refresh_token, refresh_token)) &&
-            (identical(other.expiry_date, expiry_date) ||
-                const DeepCollectionEquality()
-                    .equals(other.expiry_date, expiry_date)) &&
-            (identical(other.access_token, access_token) ||
-                const DeepCollectionEquality()
-                    .equals(other.access_token, access_token)) &&
-            (identical(other.token_type, token_type) ||
-                const DeepCollectionEquality()
-                    .equals(other.token_type, token_type)) &&
-            (identical(other.id_token, id_token) ||
-                const DeepCollectionEquality()
-                    .equals(other.id_token, id_token)) &&
->>>>>>> dev
             (identical(other.scope, scope) ||
                 const DeepCollectionEquality().equals(other.scope, scope)));
   }
@@ -331,19 +221,11 @@ class _$_GoogleUserCredentials implements _GoogleUserCredentials {
   @override
   int get hashCode =>
       runtimeType.hashCode ^
-<<<<<<< HEAD
       const DeepCollectionEquality().hash(refreshToken) ^
       const DeepCollectionEquality().hash(expiryDate) ^
       const DeepCollectionEquality().hash(accessToken) ^
       const DeepCollectionEquality().hash(tokenType) ^
       const DeepCollectionEquality().hash(idToken) ^
-=======
-      const DeepCollectionEquality().hash(refresh_token) ^
-      const DeepCollectionEquality().hash(expiry_date) ^
-      const DeepCollectionEquality().hash(access_token) ^
-      const DeepCollectionEquality().hash(token_type) ^
-      const DeepCollectionEquality().hash(id_token) ^
->>>>>>> dev
       const DeepCollectionEquality().hash(scope);
 
   @JsonKey(ignore: true)
@@ -360,26 +242,17 @@ class _$_GoogleUserCredentials implements _GoogleUserCredentials {
 
 abstract class _GoogleUserCredentials implements GoogleUserCredentials {
   factory _GoogleUserCredentials(
-<<<<<<< HEAD
       {@JsonKey(name: 'refresh_token') required String refreshToken,
       @JsonKey(name: 'expiry_date') required int expiryDate,
       @JsonKey(name: 'access_token') required String accessToken,
       @JsonKey(name: 'token_type') required String tokenType,
       @JsonKey(name: 'id_token') required String idToken,
-=======
-      {required String refresh_token,
-      required int expiry_date,
-      required String access_token,
-      required String token_type,
-      required String id_token,
->>>>>>> dev
       required String scope}) = _$_GoogleUserCredentials;
 
   factory _GoogleUserCredentials.fromJson(Map<String, dynamic> json) =
       _$_GoogleUserCredentials.fromJson;
 
   @override
-<<<<<<< HEAD
   @JsonKey(name: 'refresh_token')
   String get refreshToken;
   @override
@@ -394,17 +267,6 @@ abstract class _GoogleUserCredentials implements GoogleUserCredentials {
   @override
   @JsonKey(name: 'id_token')
   String get idToken;
-=======
-  String get refresh_token;
-  @override
-  int get expiry_date;
-  @override
-  String get access_token;
-  @override
-  String get token_type;
-  @override
-  String get id_token;
->>>>>>> dev
   @override
   String get scope;
   @override
