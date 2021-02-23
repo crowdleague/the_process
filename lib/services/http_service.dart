c26c7b8015222d2207f494e2b102e1bb8f71a939
Nick Meinhold <nick.meinhold@gmail.com>
2021-02-23 13:08:29 +1100

    Move section creation to HttpService, update tests
    - adding a new service reqquired a lot of changes to other tests,
    have added a task to DRY us up
    - added a http service, uses package:http to hit microservice endpoint
    - added mocks for new service and it's actor, the http client
    - updated tests to use new mocks

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:http/http.dart' as http;
 
 class HttpService {
@@ -10,10 +8,10 @@ class HttpService {
   Future<void> createSection({required String name}) async {
     final queryParameters = {'name': name};
     final uri = Uri.https(
-        'https://dartimageresize-v6exb2sdca-uc.a.run.app', '', queryParameters);
+        'dartimageresize-v6exb2sdca-uc.a.run.app', '', queryParameters);
     final response = await _client.post(uri);
-    if (response.statusCode != HttpStatus.ok) {
-      throw response.statusCode;
+    if (response.statusCode != 200) {
+      throw '${response.statusCode} : ${response.reasonPhrase}';
     }
   }
 }
