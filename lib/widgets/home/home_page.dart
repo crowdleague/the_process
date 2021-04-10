3e733db7397ed712c42f24936892d8c0814ed483
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-10 11:24:37 +1000

    Use constraints from LayoutBuilder to switch modes

--- lib/widgets/home/home_page.dart
+++ lib/widgets/home/home_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:the_process/widgets/home/project-selection/project_selection_page.dart';
+import 'package:the_process/widgets/home/section-detail/section_detail_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return ProjectSelectionPage();
+      if (constraints.maxWidth < 500) return ProjectSelectionPage();
+      return Row(
+        children: [
+          Container(width: 300, child: ProjectSelectionPage()),
+          SectionDetailPage()
+        ],
+      );
     });
   }
 }
