14d4fd884e3ac29be924bdb97c39c7ddf099c29b
Nick Meinhold <nick.meinhold@gmail.com>
2021-07-03 15:44:24 +1000

    Start moving relevant tests in redfire

--- test/test-doubles/services/service_test_doubles.dart
+++ test/test-doubles/services/service_test_doubles.dart
@@ -1,8 +1,5 @@
 import 'package:mockito/annotations.dart';
-import 'package:the_process/services/auth_service.dart';
-import 'package:the_process/services/database_service.dart';
 import 'package:the_process/services/http_service.dart';
-import 'package:the_process/services/platform_service.dart';
 
-@GenerateMocks([AuthService, DatabaseService, HttpService, PlatformService])
+@GenerateMocks([HttpService])
 class ServiceTestDoubles {}
